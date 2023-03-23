14742b447f6c468342a43d4680b9829ba8025e60
Riley Dellios <delcari@protonmail.com>
2023-03-23 21:39:40 +1100

    ENHANCE: Update highlight - Add interaction cooldown

--- src/main/java/au/rdellios/bdiagriculture/InspectTreePlan.java
+++ src/main/java/au/rdellios/bdiagriculture/InspectTreePlan.java
@@ -5,6 +5,9 @@ import jadex.bdiv3.annotation.PlanAPI;
 import jadex.bdiv3.annotation.PlanBody;
 import jadex.bdiv3.annotation.PlanCapability;
 import jadex.bdiv3.runtime.IPlan;
+import jadex.extension.envsupport.environment.ISpaceObject;
+
+import java.awt.*;
 
 @Plan
 public class InspectTreePlan {
@@ -14,11 +17,20 @@ public class InspectTreePlan {
     @PlanAPI
     protected IPlan rplan;
 
+
+    //Plan Proposal ------------------------------------------------------------------
+    //Move diagonal around object
+    //Update BeliefBase - Information about Tree - proposed job
+    //Send message to another agent - about job
+    //--------------------------------------------------------------------------------
     @PlanBody
-    public void body()
-    {
-        //Move diagonal around object
-        //Update BeliefBase - Information about Tree - proposed job
-        //Send message to another agent - about job
+    public void body(ISpaceObject targetTree) {
+        System.out.println("Starting InspectTreePlan...");
+        //Log the time of the last interaction with the tree
+        targetTree.setProperty("lastInteraction", System.currentTimeMillis());
+        //Highlight the tree
+        ScoutAgent.updateHighlight(targetTree, new Color(149, 255, 83, 85));
+        System.out.println("InspectTreePlan: Tree Inspected");
     }
 }
+
