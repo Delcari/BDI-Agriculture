2b82e569ffadede3c10d52aa7fcb1e99d1327251
Riley Dellios <delcari@protonmail.com>
2023-03-23 21:36:11 +1100

    ENHANCE: Update logging level to assist with debugging

--- src/main/java/au/rdellios/bdiagriculture/Main.java
+++ src/main/java/au/rdellios/bdiagriculture/Main.java
@@ -5,12 +5,14 @@ import jadex.base.PlatformConfigurationHandler;
 import jadex.base.Starter;
 import jadex.bridge.IExternalAccess;
 import jadex.bridge.service.types.cms.CreationInfo;
+import java.util.logging.Level;
 
 public class Main {
     public static void main(String[] args) {
         IPlatformConfiguration platformConfiguration;
         platformConfiguration = PlatformConfigurationHandler.getDefault();
         //platformConfiguration.getGui();
+        platformConfiguration.setLoggingLevel(Level.WARNING);
         IExternalAccess platform = Starter.createPlatform(platformConfiguration).get();
         CreationInfo ci = new CreationInfo().setFilename("src/main/java/au/rdellios/bdiagriculture/bdiAgriculture.application.xml");
         platform.createComponent(ci).get();
